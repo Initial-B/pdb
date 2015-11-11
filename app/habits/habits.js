ede9ac148710735d2e7447ee2fc99ed5bf64b909
unknown <U1J469@GP132400.GEICO.corp.net>
2015-11-11 09:47:41 -0500

    imported angular-charts + test code

--- app/habits/habits.js
+++ app/habits/habits.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('pdb.habits', [])
-
+//angular.module('pdb.habits', ['chart.js'])
+angular.module('pdb.habits', ['chart.js'])
 .controller('HabitsCtrl', ['$scope', 'habitsAPI',
 	function($scope, habitsAPI) {
 		
@@ -25,5 +25,16 @@ angular.module('pdb.habits', [])
 				}
 			);
 		};
-
-}]);
+		
+		//TEST: chart.js test vals
+		  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+		  $scope.series = ['Series A', 'Series B'];
+		  $scope.data = [
+			[65, 59, 80, 81, 56, 55, 40],
+			[28, 48, 40, 19, 86, 27, 90]
+		  ];
+		  $scope.onClick = function (points, evt) {
+			console.log(points, evt);
+		  };
+	}
+]);
