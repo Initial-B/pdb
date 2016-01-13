56da9f70b9f7cddc7465a98364b0740353df2985
unknown <U1J469@GP132400.GEICO.corp.net>
2016-01-13 10:59:38 -0500

    chart component contd 2

--- app/habits2/habits-linechart-component-directive.js
+++ app/habits2/habits-linechart-component-directive.js
@@ -3,12 +3,89 @@ angular.module('pdb.habits2')
 //	function($scope, habitsAPI, userAPI) {
 .controller('ChartComponentController', ['habitsAPI', 'userAPI',
 	function(habitsAPI, userAPI) {
-		this.habitTimeframe = "30";//days prior to today
+		var _this = this;
+		this.habitTimeframe = 30;//days prior to today
 		this.filter = 'daily';//daily or moving2WeekAverage
+		this.habitsLineChart = null;
 		
-		this.updateChart = function updateChart(){
+		this.update = function updateChart(){
+			var startDate = '0000-00-00';//default startDate ("all time")
+			var recentHabitLogs = null;
+			if(habitTimeframe){
+				startDate = habitsAPI.daysAgoToDate(habitTimeframe);
+			}
+			//get habit logs then update chart
+			habitsAPI.getHabitLogs(startDate).then(
+				function(response){
+					if(response
+					&& response.data
+					&& response.data['responseCode'] == 'success'){
+						recentHabitLogs = response.data['habitLogs'];
+					}//else display some error message?
+					var labels = [];
+					var data = [];
+					var count = 0;
+					for(var key in recentHabitLogs){
+						labels[count] = recentHabitLogs[key]['logDate'];
+						data[count] = recentHabitLogs[key]['score'];
+						count++;
+					}
+					_this.updateChart(labels, data);
+					
+				}
+			);
+		};
+				
+		this.test1 = function test1(){
+			setDataType('average');
+			console.log('habitTimeframe: ' + habitTimeframe
+			+ ' filter: ' + filter);
+		};
+		
+	//======== private functions ============
+		function updateChart(labels, data){
 			console.log('updating chart component');
-		}.bind(this);
+			//destroy old chart instance, if exist
+			if(habitsLineChart){
+				habitsLineChart.destroy();
+			}
+			habitsLineChart = new Chart(ctx, {
+				type: 'line',
+				data: {
+					labels: labels,
+					datasets: [{
+						label: 'Habit Score',
+						data: data
+					}]
+				},
+				options:{
+					legend:{display: false},
+					scales:{
+						yAxes:[{ticks:{beginAtZero:true}}],
+						xAxes:[{
+							ticks:{
+								autoSkip: false, //let the callback do label skipping
+								maxRotation: 45,
+								//called on tick, return value is set as label
+								callback: function(tickValue, index, ticks) {
+									//if there are more than 8 labels, only show every 4th
+									if(ticks.length > 8){
+										if((index % 4) === 1
+										|| index === 1){
+											return tickValue;
+										}else{
+											return '';
+										}
+									}else{
+										return tickValue;
+									}
+								}
+							}
+						}]
+					}
+				}
+			});
+		};
 		
 		function setHabitTimeframe(daysAgo){
 			habitTimeframe = parseInt(daysAgo);
@@ -17,12 +94,6 @@ angular.module('pdb.habits2')
 		
 		function setFilter(f){
 			filter = f;
-		};
-		
-		this.test1 = function test1(){
-			setDataType('average');
-			console.log('habitTimeframe: ' + habitTimeframe
-			+ ' filter: ' + filter);
 		};
 	}
 ])
